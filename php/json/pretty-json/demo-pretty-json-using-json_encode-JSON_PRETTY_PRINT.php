1ad9231bf2a450fa5f3c142b2906c9fd8f32c881
truongphucuong <30462167+truongphucuong@users.noreply.github.com>
2017-11-03 01:16:49 +0700

    add mode for file_put_contents()

--- php/json/pretty-json/demo-pretty-json-using-json_encode-JSON_PRETTY_PRINT.php
+++ php/json/pretty-json/demo-pretty-json-using-json_encode-JSON_PRETTY_PRINT.php
@@ -8,3 +8,6 @@ $arr = [
 $str = json_encode($arr, JSON_PRETTY_PRINT);
 
 file_put_contents('data.json', $str);
+
+/* FILE_APPEND: Để giữ lại nội dung trước đó mà không bị ghi đè lên */
+file_put_contents('data.json', $str , FILE_APPEND );
